cacc42ddb8d323ee63b589eabc4dd17f78c5ec52
Tú Nguyễn <tusgrace1306@gmail.com>
2023-09-22 15:07:19 +0700

    remove node module

--- js/contract/basenetwork/config.js
+++ js/contract/basenetwork/config.js
@@ -225,7 +225,7 @@ $.CONFIG.prototype = (function() {
             'weth': '0x8114b91c6Ae47d1679b7A32CaeCA473cbab9C14A',
             'cbeth': '0x3563fA251beEa49f96bD1FCD9eF2CBbE9FA88343',
             'dai': '0xE4DcDbff9f336bF236Dfa3506165f7C946522668',
-            'xbot': '0x50c4252C37e9C2652561ee2De69A11Fd89b65e0e',
+            'xbot': '0xf822758ab15278661EF66F8F461bE67f100C34c6',
         },
         8453: {//Base network
             'usdbc': '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA',
