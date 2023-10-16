5201b0094cb1988016990480b2b17b73576ed082
Tú Nguyễn <tusgrace1306@gmail.com>
2023-10-16 12:06:30 +0700

    update contract getdata

--- js/contract/basenetwork/config.js
+++ js/contract/basenetwork/config.js
@@ -92,6 +92,10 @@ $.CONFIG.prototype = (function() {
                     version: 1,
                 }
             },
+            xbot: {
+                contract: "0xf55d10baea959a54afb9f4e4e05c52cc8cc8ecf1",
+                data: "0x3aF7fD5E1160170E6c049556BA8c73e7849d9de8",
+            }
         },
         8453: {
             breezySwapV1: {
